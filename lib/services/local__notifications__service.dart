9bd96db9c56dfd2bde87a9be2a4061b70255c4b9
anasfik <ffikhi.aanas@gmail.com>
2022-08-14 16:08:51 +0100

    added scheduled notification for test

--- lib/services/local__notifications__service.dart
+++ lib/services/local__notifications__service.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_field, unused_local_variable
 
 import 'dart:math';
+import 'dart:math';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
@@ -10,6 +11,12 @@ class NotificationService {
   // Getting instance of the notification plugin
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
+
+  // Create the notification with the android settings
+  NotificationDetails platformChannelSpecifics = const NotificationDetails(
+    // you can add other platform but for now, I will use android
+    android: _androidNotificationDetails,
+  );
 
 // Specifying the android settings details
   static const AndroidNotificationDetails _androidNotificationDetails =
@@ -55,16 +62,32 @@ class NotificationService {
       macOS: null,
     );
 
-    // init this, when I will understand this, i'll explain hah
+    // Init this, when I will understand this, i'll explain hah
     tz.initializeTimeZones();
 
-    // init the big one
+    // Init the big one
     await flutterLocalNotificationsPlugin.initialize(
       initializationSettings,
     );
 
-    //send test notification on app open
-    await sendNot();
+    /// only for tests, in production comment or delete it
+    //  Instant notif on app open
+    await createInstantNotification(
+      Random().nextInt(1000),
+      'test title',
+      'test description',
+    );
+
+    // schedule notification for the next 5 secondes
+    createScheduledNotification(
+      Random().nextInt(1000),
+      'test title',
+      'test description',
+      // this will show it after 5 secondes from the current time
+      tz.TZDateTime.now(tz.local).add(
+        const Duration(seconds: 5),
+      ),
+    );
   }
 
   Future<void> requestIOSpermission() async {
@@ -78,15 +101,32 @@ class NotificationService {
         );
   }
 
-  Future sendNot() async {
-    const NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: _androidNotificationDetails);
+  Future<void> createInstantNotification(
+    int id,
+    String? title,
+    String? description,
+  ) async {
+    // Create the notification
     await flutterLocalNotificationsPlugin.show(
-      Random().nextInt(9000),
-//NOTIFICATION ID - SHOULD BE UNIQUE/RANDOM
-      'Hello',
-      'World!',
+      id,
+      title,
+      description,
+      // this is from the service class(this class)
       platformChannelSpecifics,
+    );
+  }
+
+  Future<void> createScheduledNotification(
+      int id, String? title, String? description, dynamic tzDateTime) async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      description,
+      tzDateTime,
+      platformChannelSpecifics,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
     );
   }
 }
