328c832ccf64fc069ecd888f9a39e3d1925b5063
anasfik <ffikhi.aanas@gmail.com>
2022-08-22 02:23:41 +0100

    completed delete all notificationd feature

--- lib/controllers/settings__controller/delete__all__setting.dart
+++ lib/controllers/settings__controller/delete__all__setting.dart
@@ -6,16 +6,30 @@ import '../../services/local__notifications__service.dart';
 import '../helpers__controllers/dialogs__controller.dart';
 
 class DeleteAllSetting extends GetxController {
-  
   final DialogsController dialogsController = Get.put(DialogsController());
   deleteAll() {
-  Box newNotificationsBox =
-      Hive.box<NewItemNotifcationModel>('newNotificationsBox');
-    newNotificationsBox.clear();
-    NotificationService().cancelAllNotifications();
+    Box newNotificationsBox =
+        Hive.box<NewItemNotifcationModel>('newNotificationsBox');
 
-    
-    // showing snackbar
-    dialogsController.showSnackbar("all notifications deleted successfully");
+    // check first if it's already empty
+    if (newNotificationsBox.isEmpty) {
+      dialogsController.showInfo("Already empty",
+          "there is not notification saved yet, you can schedule new notification from the home page");
+      return;
+    }
+
+    // else show confirm
+    dialogsController.showConfirmWithActions(
+        "are you sure you want to delete/Cancel all your notifications ?",
+        "delete", () async {
+      await newNotificationsBox.clear();
+      NotificationService().cancelAllNotifications();
+
+      // Going back
+      Get.back();
+
+      // showing snackbar
+      dialogsController.showSnackbar("all notifications deleted successfully");
+    });
   }
 }
